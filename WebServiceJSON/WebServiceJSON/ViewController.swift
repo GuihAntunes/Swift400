41a148ebde70c817c9460c7c960fa486ad0a3b45
Swift <swift@iMac-de-QuaddroAdm-3.local>
2017-04-01 16:54:44 -0300

    Fixing UI update problem

--- WebServiceJSON/WebServiceJSON/ViewController.swift
+++ WebServiceJSON/WebServiceJSON/ViewController.swift
@@ -8,18 +8,82 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var cepTextField: UITextField!
+    @IBOutlet weak var logradouroTextField: UITextField!
+    @IBOutlet weak var bairroTextField: UITextField!
+    @IBOutlet weak var cidadeTextField: UITextField!
+    @IBOutlet weak var ufTextField: UITextField!
+    
+    // MARK: Properties
+    var dicionarioDados = [String : String]()
+    
+	// MARK: View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.cepTextField.delegate = self
+        
     }
+    
+    // MARK: - UITextFieldDelegate Methods
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        
+        if textField == self.cepTextField && !(self.cepTextField.text!.isEmpty) {
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+                
+                let url = URL(string: "https://viacep.com.br/ws/\(textField.text!)/json/")
+                
+                let sessao = URLSession(configuration: URLSessionConfiguration.default)
+                
+                let tarefa : URLSessionDataTask = sessao.dataTask(with: url!, completionHandler: { (data, response, error) in
+                    
+                    if error == nil {
+                        
+                        
+                        guard let _ = data else {
+                            return
+                        }
+                        
+                        do{
+                            self.dicionarioDados = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as! [String : String]
+                            
+                            DispatchQueue.main.async {
+                                
+                                self.cepTextField.text = self.dicionarioDados["cep"] ?? ""
+                                self.logradouroTextField.text = self.dicionarioDados["logradouro"] ?? ""
+                                self.bairroTextField.text = self.dicionarioDados["bairro"] ?? ""
+                                self.cidadeTextField.text = self.dicionarioDados["localidade"] ?? ""
+                                self.ufTextField.text = self.dicionarioDados["uf"] ?? ""
+                                
+                            }
+                            
+                            
+
+                            
+                        }catch{}
+                        
+                        
+                    }
+                    
+                    
+                    
+                })
+                
+                tarefa.resume()
+                
+            
+                
+                print(self.dicionarioDados)
+                
+            
+                
+        }
+        
     }
-
 
 }
 
