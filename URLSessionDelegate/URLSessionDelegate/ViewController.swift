aca97aa863d18151d5ff074eb2ac34aff3dfe94f
Swift <swift@iMac-de-QuaddroAdm-3.local>
2017-04-08 13:14:48 -0300

    Creating project and layout

---
+++ URLSessionDelegate/URLSessionDelegate/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  URLSessionDelegate
+//
+//  Created by Swift on 08/04/17.
+//  Copyright © 2017 Swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    // MARK: - Propriedades
+    
+    // MARK: View Cycle Life
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    // MARK: - Métodos
+    
+    // MARK: - Actions
+    @IBAction func fazerRequisicaoGet(_ sender: UIButton) {
+    }
+    @IBAction func fazerRequisicaoPut(_ sender: UIButton) {
+    }
+    @IBAction func fazerRequisicaoPost(_ sender: UIButton) {
+    }
+    @IBAction func fazerRequisicaoDelete(_ sender: UIButton) {
+    }
+
+}
+
