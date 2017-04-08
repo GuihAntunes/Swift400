bdf6ef58730efde497f4eb5010e29fae3eab2b1d
Swift <swift@iMac-de-QuaddroAdm-3.local>
2017-04-08 10:12:57 -0300

    Creating first screen

--- DesafioFIPEAPI/DesafioFIPEAPI/ViewController.swift
+++ DesafioFIPEAPI/DesafioFIPEAPI/ViewController.swift
@@ -8,18 +8,121 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var tableViewMarcas: UITableView!
+    
+    // MARK: - Properties
+    var arrayElementos : [[String:AnyObject]] = []
+    
+    // MARK: View Cycle Life
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.setup()
+        
+        self.fazerRequisicao()
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: - General Methods
+    func setup() {
+        
+        self.tableViewMarcas.delegate = self
+        self.tableViewMarcas.dataSource = self
+        
     }
-
-
+    
+    func fazerRequisicao() {
+        
+        // Criando a URL
+        let url = URL(string: "http://fipeapi.appspot.com/api/1/carros/marcas.json")!
+        
+        // Criando a session
+        let session = URLSession(configuration: URLSessionConfiguration.default)
+        
+        let task : URLSessionDataTask = session.dataTask(with: url) { (data, response, error) in
+            
+            if error == nil{
+                
+                if let dataTemp = data {
+                    
+                    do{
+                        
+                        self.arrayElementos = try JSONSerialization.jsonObject(with: dataTemp, options: .allowFragments) as! [[String:AnyObject]]
+                        
+                        print(self.arrayElementos)
+                        
+                        DispatchQueue.main.async {
+                            
+                            self.tableViewMarcas.reloadData()
+                            
+                        }
+                        
+                    }catch{}
+                    
+                }else{
+                    
+                    self.criaAlerta(titulo: "Errrooou!", mensagem: "Xiii, data nula!")
+                    
+                }
+                
+            }else{
+                
+                self.criaAlerta(titulo: "Errrroou!", mensagem: "Xiii, erro na requisição!")
+                
+            }
+            
+        }
+        
+        task.resume()
+        
+    }
+    
+    func criaAlerta(titulo : String, mensagem : String) {
+        
+        let alerta = UIAlertController(title: titulo, message: mensagem, preferredStyle: .alert)
+        
+        let acaoOk = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        
+        alerta.addAction(acaoOk)
+        
+        self.present(alerta, animated: true, completion: nil)
+        
+    }
+    
+    // MARK: - Actions
+    
+    // MARK: - Métodos de UITableViewDataSource
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.arrayElementos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableViewMarcas.dequeueReusableCell(withIdentifier: "celula", for: indexPath)
+        
+        //
+        if let valor = (self.arrayElementos[indexPath.row]["fipe_name"]) {
+            
+            cell.textLabel?.text = "\(valor)"
+            
+        }
+        
+        
+        //
+        
+        return cell
+        
+    }
+    
+    // MARK: - Métodos de UITableViewDelegate
+    
 }
 
