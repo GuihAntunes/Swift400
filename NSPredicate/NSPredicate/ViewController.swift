0e56598977c0476c1687a7c03c04c0744ecccec8
Swift <swift@iMac-de-QuaddroAdm-3.local>
2017-04-22 16:42:24 -0300

    Studying predicate

--- NSPredicate/NSPredicate/ViewController.swift
+++ NSPredicate/NSPredicate/ViewController.swift
@@ -66,8 +66,60 @@ class ViewController: UIViewController {
         let arrayFiltradoDiferente = (self.arrayInteiros as NSArray).filtered(using: predicateDiferente)
         print(arrayFiltradoDiferente)
         
+        // BETWEEN: Intervalo de valores
+        
+        let predicateBetween = NSPredicate(format: "self BETWEEN {1,6}")
+        let arrayFiltradoBetween = (self.arrayInteiros as NSArray).filtered(using: predicateBetween)
+        print(arrayFiltradoBetween)
+        
         // ----------
         
+        // ---------- Comparações Compostas Básicas ---------- 
+        
+        // AND, &&: Operador lógico AND
+        
+        let predicateAnd = NSPredicate(format: "self BETWEEN {1,6} && self > 3")
+        let arrayFiltradoAnd = (self.arrayInteiros as NSArray).filtered(using: predicateAnd)
+        print(arrayFiltradoAnd)
+        
+        // OR, || : Operador lógico OR
+        
+        let predicateOr = NSPredicate(format: "self BETWEEN {1,3} || self > 6")
+        let arrayFiltradoOr = (self.arrayInteiros as NSArray).filtered(using: predicateOr)
+        print(arrayFiltradoOr)
+        
+        // NOT, ! : Operador Kógico NOT
+        
+        let predicateNot = NSPredicate(format: "!(self BETWEEN {1,3})")
+        let arrayFiltradoNot = (self.arrayInteiros as NSArray).filtered(using: predicateNot)
+        print(arrayFiltradoNot)
+        
+        // ----------
+        
+        // ---------- Comparações de Strings ---------- 
+        
+        // BEGINSWITH - Inicia com determinada expressão - [c] ignora "case sensitive" = [d] trata como iguais caracteres especiais. Acentuação, por exemplo
+        
+        let argumentoBeginsWith = "l"
+        let predicateBEGINSWITH = NSPredicate(format: "self BEGINSWITH[cd] %@", argumentoBeginsWith)
+        let arrayFiltradoBEGINSWITH = (self.arrayStrings as NSArray).filtered(using: predicateBEGINSWITH)
+        print(arrayFiltradoBEGINSWITH)
+        
+        // ENDSWITH - Finaliza com determinada expressão - [c] ignora "case sensitive" = [d] trata como iguais caracteres especiais. Acentuação, por exemplo
+        
+        let argumentoENDSWITH = "o"
+        let predicateENDSWITH = NSPredicate(format: "self ENDSWITH[cd] %@", argumentoENDSWITH)
+        let arrayFiltradoENDSWITH = (self.arrayStrings as NSArray).filtered(using: predicateENDSWITH)
+        print(arrayFiltradoENDSWITH)
+        
+        // CONTAINS = Contém determinada expressão
+        
+        let argumentoCONTAINS = "li"
+        let predicateCONTAINS = NSPredicate(format: "self CONTAINS[cd] %@", argumentoCONTAINS)
+        let arrayFiltradoCONTAINS = (self.arrayStrings as NSArray).filtered(using: predicateCONTAINS)
+        print(arrayFiltradoCONTAINS)
+
+    
     }
 
     override func didReceiveMemoryWarning() {
