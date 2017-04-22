fde223185f9f4fb18ca94c42e0284ee41d7ccc7c
Swift <swift@iMac-de-QuaddroAdm-3.local>
2017-04-22 09:43:24 -0300

    Creating project and importing sqlite

---
+++ SQLitePrincipaisComandos/SQLitePrincipaisComandos/Header.h
@@ -0,0 +1,14 @@
+//
+//  Header.h
+//  SQLitePrincipaisComandos
+//
+//  Created by Swift on 22/04/17.
+//  Copyright © 2017 Swift. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#import <sqlite3.h>
+
+#endif /* Header_h */
---
+++ SQLitePrincipaisComandos/SQLitePrincipaisComandos/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  SQLitePrincipaisComandos
+//
+//  Created by Swift on 22/04/17.
+//  Copyright © 2017 Swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    // MARK: - Propriedades
+    
+    // MARK: View Cycle Life
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Métodos
+    
+    // MARK: - Actions
+    
+    @IBAction func selectTudo(_ sender: UIButton) {
+        
+        
+        
+    }
+    
+    @IBAction func selectWhere(_ sender: UIButton) {
+        
+        
+        
+    }
+    
+    @IBAction func selectWhereLike(_ sender: UIButton) {
+        
+        
+        
+    }
+    
+    @IBAction func updateRegistro(_ sender: UIButton) {
+        
+        
+        
+    }
+    
+    @IBAction func deleteRegistro(_ sender: UIButton) {
+        
+        
+        
+    }
+
+    
+}
+
